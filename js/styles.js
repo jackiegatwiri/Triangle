67c9426dc9c348ff98912edb09b00574740ccdbd
jackiegatwiri <jaciegatz@gmail.com>
2019-02-15 17:01:22 +0300

    add array

--- js/styles.js
+++ js/styles.js
@@ -3,6 +3,7 @@ function calculate() {
   var firstValue = parseInt(document.getElementById("firstValue").value);
   var secondValue = parseInt(document.getElementById("secondValue").value);
   var thirdValue = parseInt(document.getElementById("thirdValue").value);
+  var array = [firstValue, secondValue, thirdValue];
 
 
 }
