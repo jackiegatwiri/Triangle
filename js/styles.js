7d4c852d0f2b4178f018bf6d9751f77920787032
jackiegatwiri <jaciegatz@gmail.com>
2019-02-15 16:43:37 +0300

    link html and css

---
+++ js/styles.js
@@ -0,0 +1,21 @@
+
+function calculate() {
+
+  var firstValue = parseInt(document.getElementById("firstValue").value);
+  var secondValue = parseInt(document.getElementById("secondValue").value);
+  var thirdValue = parseInt(document.getElementById("thirdValue").value);
+  var array = [firstValue, secondValue, thirdValue];
+
+  if ((firstValue + secondValue) <= thirdValue || (firstValue + thirdValue) <= secondValue || (secondValue + thirdValue) <= firstValue) {
+    alert("this is not a triangle");
+  }
+  if ((firstValue === secondValue) && (firstValue === thirdValue) && (thirdValue === secondValue)) {
+    alert("this is an equilateral");
+  }
+  else if ((firstValue === secondValue) && (firstValue !== thirdValue) || (secondValue === firstValue) && (secondValue !== thirdValue) || (thirdValue === firstValue) && (thirdValue !== secondValue)) {
+    alert("this is an isosceles");
+  }
+  else {
+    alert("this is a scalene");
+  }
+}
