64ae727971422aafd1015a6fca1d758ec31d3f0b
jackiegatwiri <jaciegatz@gmail.com>
2019-02-15 17:02:23 +0300

    add first is condition

--- js/styles.js
+++ js/styles.js
@@ -4,6 +4,9 @@ function calculate() {
   var secondValue = parseInt(document.getElementById("secondValue").value);
   var thirdValue = parseInt(document.getElementById("thirdValue").value);
   var array = [firstValue, secondValue, thirdValue];
+  if ((firstValue + secondValue) <= thirdValue || (firstValue + thirdValue) <= secondValue || (secondValue + thirdValue) <= firstValue) {
+    alert("this is not a triangle");
+  }
 
 
 }
