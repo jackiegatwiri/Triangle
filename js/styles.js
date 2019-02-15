80de01cf5aa19ea0d0b55e3e8bf775fd3c58e557
jackiegatwiri <jaciegatz@gmail.com>
2019-02-15 16:57:41 +0300

    create getElementById for the second value

--- js/styles.js
+++ js/styles.js
@@ -1,6 +1,7 @@
 
 function calculate() {
   var firstValue = parseInt(document.getElementById("firstValue").value);
+  var secondValue = parseInt(document.getElementById("secondValue").value);
 
 
 }
